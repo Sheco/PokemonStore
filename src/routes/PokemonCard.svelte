3d36a51c8a9741da79bdb1a86154af79be425685
Sergio Duran <sergio@tiene.info>
2023-08-29 16:00:45 -0600

    Improve responsive layout

--- src/routes/PokemonCard.svelte
+++ src/routes/PokemonCard.svelte
@@ -2,7 +2,7 @@
 	import { price, statName } from "$lib";
 	export let pokemon:Pokemon
 </script>
-<div class="card col-3 mt-2 ms-2">
+<div class="card col-md-3 col-sm-12 mt-2 ms-2">
 	<img src="{pokemon.sprites.front_default}" class="card-img-top" alt="">
 	<div class="card-body">
 		<h5 class="card-title text-center">{pokemon.name} <span class="fw-bold float-end">${price(pokemon)}</span></h5>
