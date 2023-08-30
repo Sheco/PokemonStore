947d2fb2a03b86aa25129b1c2fca42f82f992397
Sergio Duran <sergio@tiene.info>
2023-08-30 11:11:38 -0600

    Bump minor version

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -6,7 +6,7 @@
 <header class="d-flex flex-wrap justify-content-center py-1 mb-4 border-bottom text-bg-dark">
 	<a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
 		<svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
-		<span class="fs-4 text-light">Pokemon Store 0.1.3</span>
+		<span class="fs-4 text-light">Pokemon Store 0.1.4</span>
 	</a>
 
 	<span class="nav navbar-text me-2">
